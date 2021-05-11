540f6400c8a3394c7b6cca47fb12149c314fe450
Alexander Solovyov <alexander@solovyov.net>
2021-05-11 18:04:44 +0300

    cleanup router and fix redirection bug

--- src/piu/core/route.clj
+++ src/piu/core/route.clj
@@ -5,38 +5,41 @@
 
 
 (defn make-handler [re handler]
-  (-> (cond
-        (fn? handler)  {:get  handler
-                        :post handler
-                        :put  handler}
-        (map? handler) handler
-        :else          (throw (ex-info "Unknown handler type" {:handler handler})))
-      (assoc :names (->> (re-seq groups-re (.pattern re))
-                         (mapv (comp keyword second))))))
+  (let [names (->> (re-seq groups-re (.pattern re))
+                   (mapv (comp keyword second)))]
+    (-> (cond
+          (fn? handler)  {:get  handler
+                          :post handler
+                          :put  handler}
+          (map? handler) handler
+          :else          (throw (ex-info "Unknown handler type" {:handler handler})))
+        (assoc :names names))))
 
+
+(defn check-match [[re handler] req]
+  (when-let [m (re-matches re (:uri req))]
+    (let [func   (get handler (:request-method req))
+          params (zipmap
+                   (:names handler)
+                   (rest m))]
+      [(assoc req :path-params params)
+       func])))
+
+
+;;; API
 
 (defn router [routes]
   (mapv (fn [[re handler]] [re (make-handler re handler)]) routes))
 
 
-(defn check-match [[re handler] req]
-  (let [m      (re-matches re (:uri req))
-        func   (when m (get handler (:request-method req)))
-        params (when m (into {}
-                         (zipmap (:names handler)
-                           (rest m))))]
-    (when m
-      [(assoc req :path-params params)
-       func])))
-
-
 (defn match [router req]
-  (reduce
-    (fn [_ entry]
-      (some-> (check-match entry req)
-        reduced))
-    nil
-    router))
+  (or (reduce
+        (fn [_ entry]
+          (some-> (check-match entry req)
+            reduced))
+        nil
+        router)
+      [req nil]))
 
 
 (comment
