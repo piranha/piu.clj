e8882a9109cc95610b5bdf03c4721e518b6eb79b
Alexander Solovyov <alexander@solovyov.net>
2024-10-23 12:49:55 +0300

    change favicon to floppy disk

--- src/piu/views/base.clj
+++ src/piu/views/base.clj
@@ -30,7 +30,7 @@
      [:title "paste.in.ua"]
      [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
 
-     [:link {:rel "icon" :href "data:image/x-icon;base64,AA"}]
+     [:link {:rel "icon" :href "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💾</text></svg>"}]
      [:link {:rel "stylesheet" :href (static "main.css")}]
      [:link {:rel "stylesheet" :href (static "xcode.css")}]
      [:script {:src (static "all.js")}]]))
