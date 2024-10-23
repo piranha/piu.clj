06ee7f3f29ffb660a8987bdc7a57f8f1f3f99c08
Alexander Solovyov <alexander@solovyov.net>
2024-10-23 12:59:53 +0300

    new clojure - less code

--- .dir-locals.el
+++
@@ -1,2 +0,0 @@
-((nil
-  (cider-clojure-cli-aliases . "dev")))
--- src/user.clj
+++ src/user.clj
@@ -1,25 +1,9 @@
 (ns user
   (:require [mount.core :as mount]
-            [cemerick.pomegranate :as pomegranate]
-            [cemerick.pomegranate.aether]
-
             [piu.main]))
-
 
 (def start mount/start)
 (def stop mount/stop)
 
-
-(defn add-deps [& coordinates]
-  (let [thread (Thread/currentThread)
-        cl     (.getContextClassLoader thread)]
-    (when-not (instance? clojure.lang.DynamicClassLoader cl)
-      (.setContextClassLoader thread (clojure.lang.DynamicClassLoader. cl))))
-  (pomegranate/add-dependencies
-    :coordinates coordinates
-    :repositories (assoc cemerick.pomegranate.aether/maven-central
-                    "clojars" "https://clojars.org/repo")))
-
-
 (comment
-  (add-deps '[com.brunobonacci/mulog "0.2.0"]))
+  (add-lib '[com.brunobonacci/mulog "0.2.0"]))
